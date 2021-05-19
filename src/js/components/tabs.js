57ec6336af460d4f39d3ff4501d0876435561b70
joseantonio_andreu <joseantonio.andreu@flat101.es>
2021-05-19 17:09:49 +0200

    Tab´s functionality

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,26 +1,52 @@
+import {getViewportWidth, addClass, removeClass} from "../utils/utils";
+
 let tabsContainer = document.querySelector("#tabs");
 
-let tabTogglers = tabsContainer.querySelectorAll("a");
+let tabTogglers = tabsContainer.querySelectorAll(".tab-toggler");
 
-tabTogglers.forEach(function(toggler) {
-  toggler.addEventListener("click", function(e) {
-    e.preventDefault();
+export function addEventTabs() {
+  let width = getViewportWidth();
 
-    let tabName = this.getAttribute("href");
+  if(width < 1024) {
+    removeClass(document.getElementById('tabs'), 'hidden');
+    removeClass(document.getElementById('alojamiento-list-container'), 'view-tabs desktopTabs');
 
-    let tabContents = document.querySelector("#tab-contents");
+    addEventClickToTabs();
 
-    for (let i = 0; i < tabContents.children.length; i++) {
+    // show map
+    document.getElementById("default-tab").click();
+  } else {
+    addClass(document.getElementById('tabs'), 'hidden');
 
-      tabTogglers[i].parentElement.classList.remove("border-yellow-400", "border-b", "opacity-100");  tabContents.children[i].classList.remove("hidden");
-      if ("#" + tabContents.children[i].id === tabName) {
-        continue;
+    // show map
+    document.getElementById("default-tab").click();
+
+    removeClass(document.getElementById('alojamiento-list-container'), 'hidden');
+    addClass(document.getElementById('alojamiento-list-container'), 'view-tabs desktopTabs');
+  }
+}
+
+function addEventClickToTabs() {
+  let tabContents = document.querySelector("#tab-contents");
+
+  tabTogglers.forEach(function(toggler) {
+    toggler.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      let tabName = this.getAttribute("href");
+
+      for (let i = 0; i < tabContents.children.length; i++) {
+        tabTogglers[i].parentElement.classList.remove("border-yellow-400", "border-b", "opacity-100");
+        tabContents.children[i].classList.remove("hidden");
+
+        if ("#" + tabContents.children[i].id === tabName) {
+          continue;
+        }
+
+        tabContents.children[i].classList.add("hidden");
+
       }
-      tabContents.children[i].classList.add("hidden");
-
-    }
-    e.target.parentElement.classList.add("border-yellow-400", "border-b-4", "-mb-px", "opacity-100");
+      e.target.parentElement.classList.add("border-yellow-400", "border-b-4", "-mb-px", "opacity-100");
+    });
   });
-});
-
-document.getElementById("default-tab").click();
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,23 @@
 import '../sass/main.scss'
-import SendData from "./sendData";
-import Modal from "./components/modal";
-import Search from "./components/search";
+import SendData from './sendData';
+import Modal from './components/modal';
+import Search from './components/search';
 import './maps';
 import './components/tabs'
 
 import gpsIcon from '../images/gps.svg';
 import gpsIcon2 from '../images/gps2.svg';
+import {addEventTabs} from './components/tabs';
 
-window.addEventListener("load", function(event) {
+window.addEventListener('load', function(event) {
   SendData.init();
 });
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
   Modal.init();
   Search.init();
+  addEventTabs();
 });
+
+window.onresize = addEventTabs;
 
