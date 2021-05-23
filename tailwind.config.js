0e4bc87e6b757676bd5d0f6d96d7f538273c87df
joseantonio_andreu <joseantonio.andreu@flat101.es>
2021-05-23 14:14:16 +0200

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height: {
+        '90': '90%',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
